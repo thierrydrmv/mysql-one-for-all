e010142d9b837f42df8676eaf8a12cee6155b538
Thierry <varelathierry@gmail.com>
2022-12-06 20:16:14 -0300

    feat: req2 COUNT

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT
+    COUNT(DISTINCT c.nome) cancoes,
+    COUNT(DISTINCT a.nome) artistas,
+    COUNT(DISTINCT al.nome) albuns
+FROM
+    SpotifyClone.cancao c,
+    SpotifyClone.artista a,
+    SpotifyClone.album al;
