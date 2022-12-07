dcbd3ac8659b78dc56926283503d933a475dc5c2
Thierry <varelathierry@gmail.com>
2022-12-06 21:50:55 -0300

    feat: req4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT
+    u.nome usuario,
+    IF(
+        SUM(IF(YEAR(d.date) >= '2021', 1, 0)) = 1,
+        'Usuário ativo',
+        'Usuário inativo'
+    ) status_usuario
+FROM SpotifyClone.usuario u
+    JOIN SpotifyClone.data_reproducao d ON u.usuario_id = d.usuario_id
+GROUP BY d.usuario_id
+ORDER BY nome;
